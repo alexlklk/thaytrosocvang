cefa8d16f28b2dfe20b68ee8664ab0a0745c57bd
alexlklk <134233623+alexlklk@users.noreply.github.com>
2024-05-01 11:04:35 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var radius = 240; // how big of the radius
 var autoRotate = true; // auto rotate or not
 var rotateSpeed = -60; // unit: seconds/360 degrees
-var imgWidth = 120; // width of images (unit: px)
+var imgWidth = 90; // width of images (unit: px)
 var imgHeight = 170; // height of images (unit: px)
 
 // Link of background music - set 'null' if you dont want to play background music
